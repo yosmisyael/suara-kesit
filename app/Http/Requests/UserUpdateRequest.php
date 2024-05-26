4eaa3644e74d154161b133b89e7862ca2529da94
yosev <yosmisyael7308@gmail.com>
2024-05-26 21:39:25 +0700

    feat: Create user update request validation.

--- app/Http/Requests/UserUpdateRequest.php
+++ app/Http/Requests/UserUpdateRequest.php
@@ -3,15 +3,27 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
 
 class UserUpdateRequest extends FormRequest
 {
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'username' => Str::lower(),
+            'email' => Str::lower(),
+            'name' => Str::title(),
+        ]);
+    }
+
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -21,8 +33,24 @@ class UserUpdateRequest extends FormRequest
      */
     public function rules(): array
     {
+        $method = $this->method();
+
+        if ($method === 'PATCH')
+            return [
+                'id' => ['required', 'exists:users,id', 'uuid'],
+                'password' => [
+                    'required', 'string', 'confirmed', Password::min(8)->max(100)->letters()->mixedCase()->numbers()->symbols()
+                ],
+                'password_confirmation' => ['required', 'string', 'min:8', 'same:password'],
+            ];
+
         return [
-            //
+            'id' => ['required', 'exists:users,id', 'uuid'],
+            'name' => ['required', 'string', 'max:100'],
+            'email' =>  ['required', 'string', 'max:100', 'email'],
+            'username' => [
+                'required', 'string', 'alpha_dash:ascii', 'max:100', Rule::unique('users', 'username')->ignore($this->id)
+            ],
         ];
     }
 }
