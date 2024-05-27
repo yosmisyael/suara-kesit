291905e60d33c434f466835ed058adf55ace7204
yosev <yosmisyael7308@gmail.com>
2024-05-27 20:53:09 +0700

    feat: Change edit user page.

--- resources/views/pages/admin/user-edit.blade.php
+++ resources/views/pages/admin/user-edit.blade.php
@@ -2,35 +2,64 @@
     <x-slot:title>{{ $title }}</x-slot:title>
     <x-dashboard-shell>
 
-        <div class="mx-auto max-w-xl">
-            <form action="{{ route('admin.user.update', ['id' => $user->id]) }}" method="POST" class="space-y-5">
-                @csrf
-                @method('PUT')
-                <input type="hidden" name="id" value="{{ $user->id }}">
-                <div class="grid grid-cols-3 items-center">
-                    <label for="name" class="col-span-1 block text-sm font-medium text-gray-700">Name</label>
-                    <div class="col-span-2">
-                        <input name="name" type="text" id="name" class="block w-full rounded-md border-gray-300 shadow-sm focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500" placeholder="enter name here" value="{{ old('name') ?? $user->name }}" />
+        <div class="bg-white overflow-hidden shadow rounded-lg border">
+            <div class="px-4 py-5 sm:px-6">
+                <h3 class="text-lg leading-6 font-medium text-gray-900">
+                    User Profile
+                </h3>
+                <p class="mt-1 max-w-2xl text-sm text-gray-500">
+                    This is some information about the user.
+                </p>
+            </div>
+            <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
+                <dl class="sm:divide-y sm:divide-gray-200">
+                    <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500">
+                            Full name
+                        </dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            {{ $user->name }}
+                        </dd>
                     </div>
-                </div>
-                <div class="grid grid-cols-3 items-center">
-                    <label for="username" class="col-span-1 block text-sm font-medium text-gray-700">Username</label>
-                    <div class="col-span-2">
-                        <input name="username" type="text" id="username" class="block w-full rounded-md border-gray-300 shadow-sm focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500" placeholder="enter username here" value="{{ old('username') ?? $user->username }}" />
+                    <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500">
+                            Email address
+                        </dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            {{ $user->email }}
+                        </dd>
                     </div>
-                </div>
-                <div class="grid grid-cols-3 items-center">
-                    <label for="email" class="col-span-1 block text-sm font-medium text-gray-700">Email</label>
-                    <div class="col-span-2">
-                        <input name="email" type="email" id="email" class="block w-full rounded-md border-gray-300 shadow-sm focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500" placeholder="enter email here" value="{{ old('email') ?? $user->email }}" />
+                    <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500">
+                            Role
+                        </dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            Member
+                        </dd>
                     </div>
-                </div>
-                <div class="grid grid-cols-3 items-center">
-                    <div class="col-span-2 col-start-2">
-                        <button type="submit" class="rounded-lg border border-blue-500 bg-blue-500 px-5 py-2.5 text-center text-sm font-medium text-white shadow-sm transition-all hover:border-blue-700 hover:bg-blue-700 focus:ring focus:ring-blue-200 disabled:cursor-not-allowed disabled:border-blue-300 disabled:bg-blue-300">Register</button>
+                    <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500">
+                            Join on
+                        </dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            {{ $user->created_at }}
+                        </dd>
                     </div>
-                </div>
-            </form>
+                    <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500">
+                            Change role status
+                        </dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            <button type="submit" class="rounded-lg border border-blue-500 bg-blue-500 px-5 py-2.5 text-center text-sm font-medium text-white shadow-sm transition-all hover:border-blue-700 hover:bg-blue-700 focus:ring focus:ring-blue-200 disabled:cursor-not-allowed disabled:border-blue-300 disabled:bg-blue-300">Switch to member</button>
+                            <button type="submit" class="rounded-lg border border-blue-500 bg-blue-500 px-5 py-2.5 text-center text-sm font-medium text-white shadow-sm transition-all hover:border-blue-700 hover:bg-blue-700 focus:ring focus:ring-blue-200 disabled:cursor-not-allowed disabled:border-blue-300 disabled:bg-blue-300">Switch to Author</button>
+                        </dd>
+                    </div>
+                </dl>
+            </div>
         </div>
+        <a href="{{ route('admin.user.index') }}" class="mt-5 block w-fit rounded-lg border border-gray-700 bg-gray-700 px-5 py-2.5 text-center text-sm font-medium text-white shadow-sm transition-all hover:border-gray-900 hover:bg-gray-900 focus:ring focus:ring-gray-200 disabled:cursor-not-allowed disabled:border-gray-300 disabled:bg-gray-300">
+            <i class="fa-solid fa-arrow-left text-white"></i>
+            Back
+        </a>
     </x-dashboard-shell>
 </x-app>
