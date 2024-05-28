afd0a640ce881e53d7959e24a18353fd5273b63e
yosev <yosmisyael7308@gmail.com>
2024-05-28 14:33:03 +0700

    feat: Import precognition.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import Alpine from 'alpinejs';
 import 'flowbite/dist/flowbite.min.js';
+import Precognition from 'laravel-precognition-alpine';
 
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
+Alpine.plugin(Precognition);
 Alpine.start();
 
 window.Alpine = Alpine;
