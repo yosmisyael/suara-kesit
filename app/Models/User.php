7a81dca9c4f9b095dbcc877dff39bb6142a0c15a
yosev <yosmisyael7308@gmail.com>
2024-05-28 11:59:39 +0700

    feat: Add roles trait.

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasUuids;
+    use HasFactory, Notifiable, HasUuids, HasRoles;
 
     protected $primaryKey = 'id';
     protected $keyType = 'string';
