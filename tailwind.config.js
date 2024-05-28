81fc9fd70be8ecbc517b8a027314e1751c29b4b1
yosev <yosmisyael7308@gmail.com>
2024-05-28 14:27:07 +0700

    chore: Remove color in extend theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,7 @@ export default {
     ],
     darkMode: 'class',
     theme: {
-        extend: {
-            colors: {
-                primary: {"50":"#fff1f2","100":"#ffe4e6","200":"#fecdd3","300":"#fda4af","400":"#fb7185","500":"#f43f5e","600":"#e11d48","700":"#be123c","800":"#9f1239","900":"#881337","950":"#4c0519"}
-            }
-        },
+        extend: {},
         fontFamily: {
             'body': [
                 'Inter var',
