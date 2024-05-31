f593702ee765bfebd64432b42bb6328916a90543
yosev <yosmisyael7308@gmail.com>
2024-05-31 19:35:52 +0700

    refactor: Extract modal to livewire component.

--- resources/views/pages/admin/user.blade.php
+++ resources/views/pages/admin/user.blade.php
@@ -2,70 +2,71 @@
     <x-slot:title>User Management</x-slot:title>
     <x-dashboard-shell>
 
-        <div class="max-w-xs mb-5">
-            <div>
-                <div class="group relative">
-                    <input type="text" id="example9" class="block w-full rounded-md border-gray-300 px-10 shadow-sm transition-all hover:bg-gray-50 focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500" placeholder="Quick search..." />
-                    <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center px-2.5 text-gray-500">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
-                            <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
-                        </svg>
-                    </div>
-                    <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2.5">
-                        <span class="rounded border px-1.5 text-sm text-gray-400 shadow-sm transition-all group-hover:border-primary-500 group-hover:text-primary-500"><kbd>⌘</kbd> <kbd>K</kbd></span>
+        <div x-data="{ showModal: false }">
+
+            <div class="max-w-xs mb-5">
+                <div>
+                    <div class="group relative">
+                        <input type="text" id="example9" class="block w-full rounded-md border-gray-300 px-10 shadow-sm transition-all hover:bg-gray-50 focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500" placeholder="Quick search..." />
+                        <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center px-2.5 text-gray-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
+                                <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2.5">
+                            <span class="rounded border px-1.5 text-sm text-gray-400 shadow-sm transition-all group-hover:border-primary-500 group-hover:text-primary-500"><kbd>⌘</kbd> <kbd>K</kbd></span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="rounded-lg border border-gray-200 shadow-md">
-            <table class="w-full border-collapse bg-white text-left text-sm text-gray-500">
-                <thead class="bg-gray-50">
-                <tr>
-                    <th scope="col" class="px-6 py-4 font-medium text-gray-900">Name</th>
-                    <th scope="col" class="px-6 py-4 font-medium text-gray-900">Role</th>
-                    <th scope="col" class="px-6 py-4 font-medium text-gray-900">Join on</th>
-                    <th scope="col" class="px-6 py-4 font-medium text-gray-900">Actions</th>
-                </tr>
-                </thead>
-                <tbody class="divide-y divide-gray-100 border-t border-gray-100">
-                @foreach($users as $user)
-                    <tr class="hover:bg-gray-50">
-                        <th class="flex gap-3 px-6 py-4 font-normal text-gray-900">
-                            <div class="relative h-10 w-10">
-                                <img class="h-full w-full rounded-full object-cover object-center" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                                <span class="absolute right-0 bottom-0 h-2 w-2 rounded-full bg-green-400 ring ring-white"></span>
-                            </div>
-                            <div class="text-sm">
-                                <div class="font-medium text-gray-700">{{ $user->name }}</div>
-                                <div class="text-gray-400">{{ $user->email }}</div>
-                            </div>
-                        </th>
-                        <td class="px-6 py-4">Product Designer</td>
-                        <td class="px-6 py-4">{{ \Carbon\Carbon::parse($user->created_at)->format('d M Y') }}</td>
-                        <td class="px-6 py-4">
-                            <div class="flex justify-start gap-4">
-                                <a x-data="{ tooltip: 'Delete' }" href="{{ route('admin.user.edit', ['id' => $user->id]) }}">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6" x-tooltip="tooltip">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-                                    </svg>
-                                </a>
-                                <form action="{{ route('admin.user.delete', ['id' => $user->id]) }}" method="post">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" x-data="{ tooltip: 'Edite' }">
-
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6" x-tooltip="tooltip">
+            <div class="rounded-lg border border-gray-200 shadow-md">
+                <table class="w-full border-collapse bg-white text-left text-sm text-gray-500">
+                    <thead class="bg-gray-50">
+                    <tr>
+                        <th scope="col" class="px-6 py-4 font-medium text-gray-900">Name</th>
+                        <th scope="col" class="px-6 py-4 font-medium text-gray-900">Role</th>
+                        <th scope="col" class="px-6 py-4 font-medium text-gray-900">Join on</th>
+                        <th scope="col" class="px-6 py-4 font-medium text-gray-900">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody class="divide-y divide-gray-100 border-t border-gray-100">
+                    @foreach($users as $user)
+                        <tr class="hover:bg-gray-50">
+                            <th class="flex gap-3 px-6 py-4 font-normal text-gray-900">
+                                <div class="relative h-10 w-10">
+                                    <img class="h-full w-full rounded-full object-cover object-center" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                                    <span class="absolute right-0 bottom-0 h-2 w-2 rounded-full bg-green-400 ring ring-white"></span>
+                                </div>
+                                <div class="text-sm">
+                                    <div class="font-medium text-gray-700">{{ $user->name }}</div>
+                                    <div class="text-gray-400">{{ $user->email }}</div>
+                                </div>
+                            </th>
+                            <td class="px-6 py-4">Product Designer</td>
+                            <td class="px-6 py-4">{{ \Carbon\Carbon::parse($user->created_at)->format('d M Y') }}</td>
+                            <td class="px-6 py-4">
+                                <div class="flex justify-start gap-4">
+                                    <a href="{{ route('admin.user.edit', ['id' => $user->id]) }}">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                                        </svg>
+                                    </a>
+                                    <button type="button" @click="showModal = true">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6" >
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                         </svg>
                                     </button>
-                                </form>
-                            </div>
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
+                                </div>
+                            </td>
+                        </tr>
+                        <template x-if="showModal">
+                            <livewire:delete-user-modal :id="$user->id" />
+                        </template>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </x-dashboard-shell>
 </x-app>
