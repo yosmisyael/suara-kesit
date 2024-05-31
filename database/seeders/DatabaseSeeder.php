34be9389d5ba736ab5322affabfa78f3a420f236
yosev <yosmisyael7308@gmail.com>
2024-05-31 11:55:59 +0700

    fix: Fix seed execution order.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            RolesAndPermissionSeeder::class,
             AdminSeeder::class,
             UserSeeder::class,
-            RolesAndPermissionSeeder::class,
         ]);
     }
 }
