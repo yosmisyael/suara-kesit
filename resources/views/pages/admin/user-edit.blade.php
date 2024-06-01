93d59b752645efbb83d6f1969ca62011f523ceea
yosev <yosmisyael7308@gmail.com>
2024-06-01 23:37:43 +0700

    feat: Add alert message.

--- resources/views/pages/admin/user-edit.blade.php
+++ resources/views/pages/admin/user-edit.blade.php
@@ -1,7 +1,22 @@
 <x-app>
     <x-slot:title>{{ $title }}</x-slot:title>
     <x-dashboard-shell>
-        <div x-data="{ showModal: false }">
+        <div x-data="{ showModal: false }" class="relative">
+            <div class="w-fit left-1/2 -translate-x-1/2 absolute z-10">
+                @if(session('success'))
+                    <div class="flex rounded-md bg-green-100 p-4 text-sm text-green-500" x-cloak x-show="showAlert" x-data="{ showAlert: true }">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="mr-3 h-5 w-5 flex-shrink-0">
+                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+                        </svg>
+                        <div><b>Success Alert.</b> {{ session()->get('success') }} </div>
+                        <button class="ml-auto" x-on:click="showAlert = false">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
+                                <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+                            </svg>
+                        </button>
+                    </div>
+                @endif
+            </div>
 
             <div class="bg-white overflow-hidden shadow rounded-lg border">
                 <div class="px-4 py-5 sm:px-6">
