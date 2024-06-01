385c86e30439a74e6664834cac6739f72b8e5010
yosev <yosmisyael7308@gmail.com>
2024-06-01 23:03:10 +0700

    feat: Add method to handle author and member listing.

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -40,7 +40,7 @@ class AdminUserController extends Controller
     public function showAuthor(): Response
     {
         $users = $this->userService->getByRole('author');
-        return response()->view('pages.admin.user-member', [
+        return response()->view('pages.admin.user-author', [
             'title' => 'User | Author List',
             'users' => $users,
         ]);
