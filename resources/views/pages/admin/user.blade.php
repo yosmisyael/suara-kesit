df6c83de208cde16457ee23d47583ee27b7237da
yosev <yosmisyael7308@gmail.com>
2024-06-02 02:48:17 +0700

    feat: Change user management ui.

--- resources/views/pages/admin/user.blade.php
+++ resources/views/pages/admin/user.blade.php
@@ -3,17 +3,25 @@
     <x-dashboard-shell>
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
             <div
-                class="border-2 border-dashed border-gray-300 rounded-lg dark:border-gray-600 h-32 md:h-64"
-            ></div>
+                class="border-2 border-dashed border-gray-300 rounded-lg dark:border-gray-600 h-32 md:h-32"
+            >
+                All active users 123
+            </div>
             <div
-                class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64"
-            ></div>
+                class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-32"
+            >
+                Member Total
+            </div>
             <div
-                class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64"
-            ></div>
+                class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-32"
+            >
+                Author Total
+            </div>
             <div
-                class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64"
-            ></div>
+                class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-32"
+            >
+                Author Request
+            </div>
         </div>
         <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4">
 
