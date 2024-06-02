54cb342ebe54087d18c88cdd9fac10596b6827e7
yosev <yosmisyael7308@gmail.com>
2024-06-02 13:43:25 +0700

    feat: Create post service test.

--- tests/Feature/Services/PostServiceTest.php
+++ tests/Feature/Services/PostServiceTest.php
@@ -1,5 +1,63 @@
 <?php
 
-it('$END$', function () {
+use App\Models\Post;
+use App\Models\User;
+use App\Services\PostService;
+use Database\Seeders\PostSeeder;
+use Database\Seeders\RolesAndPermissionSeeder;
+use Database\Seeders\UserSeeder;
 
+describe('PostService', function () {
+    beforeEach(function () {
+        $this->postService = app()->make(PostService::class);
+        $this->seed([RolesAndPermissionSeeder::class, UserSeeder::class]);
+        $this->user = User::query()->where('username', '=', 'zeta')->first();
+    });
+
+    it('should be able to create post', function () {
+        expect($this->actingAs($this->user)
+            ->postService->create([
+                'title' => 'dummy',
+                'content' => 'dummy',
+            ]))->toBeTrue();
+    });
+
+    it('should be able to update post', function () {
+        $this->seed(PostSeeder::class);
+        $post = Post::query()->where('title', '=', 'example')->first();
+        expect($this->actingAs($this->user)
+            ->postService->update($post->id, [
+                'title' => 'example (updated)',
+            ]))->toBeTrue();
+    });
+
+    it('should be able to delete post', function () {
+        $this->seed(PostSeeder::class);
+        $post = Post::query()->where('title', '=', 'example')->first();
+        expect($this->actingAs($this->user)
+            ->postService->delete($post->id))->toBeTrue()
+                ->and(Post::query()->find($post->id))->toBeNull();
+    });
+
+    it('should be able to get all post', function () {
+        Post::withoutEvents(function () {
+            return Post::factory()->count(5)->create();
+        });
+
+        expect($this->actingAs($this->user)
+            ->postService->all()->count())->toBe(5);
+    });
+
+    it('should be able to get post by title', function () {
+        $this->seed(PostSeeder::class);
+        expect($this->actingAs($this->user)
+            ->postService->getByTitle('example')->count())->toBe(1);
+    });
+
+    it('should be able to get post by id', function () {
+        $this->seed(PostSeeder::class);
+        $post = Post::query()->where('title', '=', 'example')->first();
+        expect($this->actingAs($this->user)
+            ->postService->getById($post->id)->count())->toBe(1);
+    });
 });
