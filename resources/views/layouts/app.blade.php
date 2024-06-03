e07c93b19a45e99cd92164a0b9207d2f6df0c0d9
yosev <yosmisyael7308@gmail.com>
2024-06-03 13:11:32 +0700

    feat: Add faicon cdn.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <title>{{ $title }}</title>
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://kit.fontawesome.com/55050161ee.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 </head>
 <body>
