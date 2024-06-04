3e4926fd61fc2d31d86efbb1adbfc4efbcd2b9fb
yosev <yosmisyael7308@gmail.com>
2024-06-04 19:31:45 +0700

    feat: Add service's method.

--- app/Services/AuthorApplicationService.php
+++ app/Services/AuthorApplicationService.php
@@ -2,8 +2,12 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Collection;
+
 interface AuthorApplicationService
 {
+    public function all(): Collection;
+
     public function approve(string $applicationId): bool;
 
     public function deny(string $applicationId): bool;
