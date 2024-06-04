0a07809013f0758837eb41ac925e9dcf3e170ca5
yosev <yosmisyael7308@gmail.com>
2024-06-05 01:13:50 +0700

    feat: Add link to token management page.

--- resources/views/components/dashboard-sidebar.blade.php
+++ resources/views/components/dashboard-sidebar.blade.php
@@ -81,6 +81,13 @@
                         >Author Application</a
                         >
                     </li>
+                    <li>
+                        <a
+                            href="{{ route('admin.application.token') }}"
+                            class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                        >Token Management</a
+                        >
+                    </li>
                 </ul>
             </li>
             <li>
