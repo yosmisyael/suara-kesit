365c96e92be7c55b3135f6e9c9367c6ee6966ab2
yosev <yosmisyael7308@gmail.com>
2024-06-04 19:42:50 +0700

    feat: Add link to application list page.

--- resources/views/components/dashboard-sidebar.blade.php
+++ resources/views/components/dashboard-sidebar.blade.php
@@ -76,7 +76,7 @@
                     </li>
                     <li>
                         <a
-                            href="#"
+                            href="{{ route('admin.application.index') }}"
                             class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                         >Author Application</a
                         >
