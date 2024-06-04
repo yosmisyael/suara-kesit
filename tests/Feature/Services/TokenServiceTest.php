23d3d08482d023322313fc7d637922181bc316f6
yosev <yosmisyael7308@gmail.com>
2024-06-04 13:33:13 +0700

    feat: Create test for token service.

--- tests/Feature/Services/TokenServiceTest.php
+++ tests/Feature/Services/TokenServiceTest.php
@@ -1,5 +1,14 @@
 <?php
 
-it('$END$', function () {
+use App\Services\TokenService;
 
+describe('TokenService', function() {
+    beforeEach(function () {
+        $this->tokenService = app()->make(TokenService::class);
+    });
+
+    it('should be able to generate token.', function () {
+        expect($this->tokenService->create())->toBeTrue();
+    });
 });
+
