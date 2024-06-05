0d6a6ffd5e3dbc1be60efe6cd41c8100cc61b32a
yosev <yosmisyael7308@gmail.com>
2024-06-05 12:06:53 +0700

    feat: Create author application service test.

--- tests/Feature/Services/AuthorApplicationServiceTest.php
+++ tests/Feature/Services/AuthorApplicationServiceTest.php
@@ -1,5 +1,22 @@
 <?php
 
-it('$END$', function () {
+use App\Models\User;
+use App\Services\AuthorApplicationService;
+use Database\Seeders\RolesAndPermissionSeeder;
+use Database\Seeders\UserSeeder;
+use Illuminate\Support\Str;
 
+describe('AuthorApplicationService', function() {
+    beforeEach(function () {
+        $this->authorApplicationService = app()->make(AuthorApplicationService::class);
+        $this->seed([RolesAndPermissionSeeder::class, UserSeeder::class]);
+        $this->user = User::query()->where('username', '=', 'alpha')->first();
+    });
+
+    it('it should be able to make author application', function () {
+        expect($this->authorApplicationService->create([
+            'user_id' => $this->user->id,
+            'token' => Str::uuid(),
+        ]))->toBeTrue();
+    });
 });
