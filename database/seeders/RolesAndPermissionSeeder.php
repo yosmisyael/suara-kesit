e4c993a16e2200a59ee28365efd2157749e573f4
yosev <yosmisyael7308@gmail.com>
2024-06-06 10:09:48 +0700

    chore: Change imported role and permission model.

--- database/seeders/RolesAndPermissionSeeder.php
+++ database/seeders/RolesAndPermissionSeeder.php
@@ -2,10 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\Permission;
-use App\Models\Role;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class RolesAndPermissionSeeder extends Seeder
 {
