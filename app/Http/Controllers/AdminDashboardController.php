ac1bbfcc669e1cf35bbad6d82f71fccda6e47497
yosev <yosmisyael7308@gmail.com>
2024-06-06 20:00:51 +0700

    feat: Add return type of controller method.

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class AdminDashboardController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): Response
     {
         return response()->view('pages.admin.index', [
             'title' => 'Dashboard',
