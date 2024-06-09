c828e7987d14c907a768f27f7c121bd174c58213
yosev <yosmisyael7308@gmail.com>
2024-06-09 21:01:20 +0700

    test: create test for admin post controller

---
+++ tests/Feature/Controller/AdminPostControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Admin;
+use App\Models\Post;
+use Database\Seeders\DatabaseSeeder;
+
+describe('AdminPostController', function () {
+    beforeEach(function () {
+        $this->seed(DatabaseSeeder::class);
+        $this->admin = Admin::query()->first();
+    });
+
+    it('should be able to access post listing page', function () {
+        $this->actingAs($this->admin, 'admin')->get(route('admin.post.index'))
+            ->assertViewIs('pages.admin.posts');
+    });
+
+    it('should be able to access post edit page', function () {
+        $post = Post::query()->where('is_published', '=', true)->first();
+        $this->actingAs($this->admin, 'admin')->get(route('admin.post.edit', ['id' => $post->id]))
+            ->assertViewIs('pages.admin.post');
+    });
+
+    it('should be able to delete post', function () {
+        $post = Post::query()->where('is_published', '=', true)->first();
+        $this->actingAs($this->admin, 'admin')->delete(route('admin.post.delete', ['id' => $post->id]))
+            ->assertRedirect(route('pages.admin.index'))
+            ->assertSessionHas('success', 'post has been taken down.');
+    });
+});
