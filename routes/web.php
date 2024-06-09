bd74850fac1722d5ed78f2c68a97edfc602d79c3
yosev <yosmisyael7308@gmail.com>
2024-06-09 13:22:20 +0700

    style: remove slash prefix in every routes

--- routes/web.php
+++ routes/web.php
@@ -22,34 +22,34 @@ Route::prefix('control-panel')->group(function () {
 
     Route::get('/', AdminDashboardController::class)->middleware(AllowAuthenticateAdmin::class)->name('admin.dashboard');
 
-    Route::prefix('/user')->group(function () {
+    Route::prefix('user')->group(function () {
         Route::controller(AdminUserController::class)->group(function () {
             Route::get('/', 'index')->name('admin.user.index');
-            Route::get('/member', 'showMember')->name('admin.user.member');
-            Route::get('/author', 'showAuthor')->name('admin.user.author');
-            Route::get('/create', 'create')->name('admin.user.create');
-            Route::post('/store', 'store')->name('admin.user.store')->middleware(HandlePrecognitiveRequests::class);
-            Route::get('/{id}/edit', 'edit')->name('admin.user.edit');
-            Route::put('/{id}', 'update')->name('admin.user.update');
-            Route::delete('/{id}', 'destroy')->name('admin.user.delete');
+            Route::get('member', 'showMember')->name('admin.user.member');
+            Route::get('author', 'showAuthor')->name('admin.user.author');
+            Route::get('create', 'create')->name('admin.user.create');
+            Route::post('store', 'store')->name('admin.user.store')->middleware(HandlePrecognitiveRequests::class);
+            Route::get('{id}/edit', 'edit')->name('admin.user.edit');
+            Route::put('{id}', 'update')->name('admin.user.update');
+            Route::delete('{id}', 'destroy')->name('admin.user.delete');
         });
 
-        Route::prefix('/application')->controller(AdminAuthorController::class)->group(function () {
-            Route::prefix('/token')->group(function () {
+        Route::prefix('application')->controller(AdminAuthorController::class)->group(function () {
+            Route::prefix('token')->group(function () {
                 Route::get('/', 'listTokens')->name('admin.application.token');
-                Route::get('/generate', 'issueToken')->name('admin.application.token-generate');
+                Route::get('generate', 'issueToken')->name('admin.application.token-generate');
             });
             Route::get('/', 'index')->name('admin.application.index');
-            Route::get('/{id}', 'show')->name('admin.application.review');
-            Route::patch('/{id}/verify', 'handleVerification')->name('admin.application.verification');
+            Route::get('{id}', 'show')->name('admin.application.review');
+            Route::patch('{id}/verify', 'handleVerification')->name('admin.application.verification');
         });
     })->middleware(AllowAuthenticateAdmin::class);
 
-    Route::prefix('/post')->controller(AdminPostController::class)->group(function () {
+    Route::prefix('post')->controller(AdminPostController::class)->group(function () {
         Route::get('/', 'index')->name('admin.post.index');
-        Route::get('/{id}/edit', 'edit')->name('admin.post.edit');
-        Route::patch('/{id}', 'update')->name('admin.post.update');
-        Route::delete('/{id}', 'destroy')->name('admin.post.delete');
+        Route::get('{id}/edit', 'edit')->name('admin.post.edit');
+        Route::patch('{id}', 'update')->name('admin.post.update');
+        Route::delete('{id}', 'destroy')->name('admin.post.delete');
     });
 });
 
