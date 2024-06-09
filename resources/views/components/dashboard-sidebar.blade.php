07e6feecb84dd5099041b424a015419d6ea917ae
yosev <yosmisyael7308@gmail.com>
2024-06-09 14:29:50 +0700

    feat: add active effect on nav link

--- resources/views/components/dashboard-sidebar.blade.php
+++ resources/views/components/dashboard-sidebar.blade.php
@@ -119,7 +119,7 @@
                     <li>
                         <a
                             href="{{ route('admin.post.index') }}"
-                            class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                            class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 {{ request()->getUri() === route('admin.post.index') ? 'bg-gray-100' : '' }}"
                         >List</a
                         >
                     </li>
