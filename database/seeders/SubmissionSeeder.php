848e868408041070251636147458350cf805cd22
yosev <yosmisyael7308@gmail.com>
2024-06-10 13:40:43 +0700

    refactor: remove unused code

--- database/seeders/SubmissionSeeder.php
+++ database/seeders/SubmissionSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Post;
 use App\Models\Submission;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class SubmissionSeeder extends Seeder
