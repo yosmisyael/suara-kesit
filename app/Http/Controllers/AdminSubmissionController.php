af6cd489b9450c618ca3c214e7715ae8130e638b
yosev <yosmisyael7308@gmail.com>
2024-06-10 15:06:56 +0700

    feat: create admin submission controller

---
+++ app/Http/Controllers/AdminSubmissionController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Enums\Status;
+use App\Http\Requests\UpdateSubmissionRequest;
+use App\Services\SubmissionService;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class AdminSubmissionController extends Controller
+{
+    public function __construct(protected SubmissionService $submissionService)
+    {
+    }
+
+    public function index(): Response
+    {
+        return response()->view('pages.admin.submissions', [
+            'title' => 'Post | Submission List',
+            'submissions' => $this->submissionService->getByStatus(Status::Pending),
+        ]);
+    }
+
+    public function edit(string $id): Response
+    {
+        return response()->view('pages.admin.submission', [
+            'title' => 'Post | Submission Review',
+            'submission' => $this->submissionService->getById($id),
+        ]);
+    }
+
+    public function update(UpdateSubmissionRequest $request, string $id): RedirectResponse
+    {
+        $validated = $request->validated();
+
+        $result = $this->submissionService->update($id, [
+            'status' => $validated['status'],
+            'post_id' => $validated['post_id'],
+        ]);
+
+        if (!$result)
+            return redirect(route('admin.submissions.edit', ['id' => $id]))
+                ->withErrors(['error', 'An error occurred when updating submission']);
+
+        return redirect(route('admin.submissions.index', ['id' => $id]))
+            ->with('success', 'Submission updated successfully');
+    }
+}
