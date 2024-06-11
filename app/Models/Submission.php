2a7c3163a0d602ee2278277a740e0cfbbb02ec37
yosev <yosmisyael7308@gmail.com>
2024-06-11 14:53:13 +0700

    feat: define relation method to review model

--- app/Models/Submission.php
+++ app/Models/Submission.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Submission extends Model
 {
@@ -22,5 +23,10 @@ class Submission extends Model
     public function post(): BelongsTo
     {
         return $this->belongsTo(Post::class);
+    }
+
+    public function review(): HasOne
+    {
+        return $this->hasOne(Review::class);
     }
 }
