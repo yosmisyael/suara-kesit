c21a576ada40b83419dce040a70f1371d1744d33
yosev <yosmisyael7308@gmail.com>
2024-06-11 13:42:38 +0700

    chore: move foreign key from applications to tokens table

--- database/migrations/0001_01_01_000005_create_tokens_table.php
+++ database/migrations/0001_01_01_000005_create_tokens_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->uuid('token')->unique();
             $table->boolean('is_active');
+            $table->foreignId('application_id')->nullable()->constrained();
             $table->timestamps();
         });
     }
