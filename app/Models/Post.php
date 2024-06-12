f55cb4c2629878ff805d934ebb876b01976555b3
yosev <yosmisyael7308@gmail.com>
2024-06-12 12:51:48 +0700

    docs: create model docs block

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,12 +2,27 @@
 
 namespace App\Models;
 
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 
+
+/**
+ * @property string $id
+ * @property string $title
+ * @property string $content
+ * @property bool $is_published
+ * @property string $user_id
+ * @property string $slug
+ * @property User $user
+ * @property Collection $submissions
+ * @property DateTimeInterface $created_at
+ * @property DateTimeInterface $updated_at
+ */
 class Post extends Model
 {
     use HasFactory;
