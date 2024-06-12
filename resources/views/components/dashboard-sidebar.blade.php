1efa743017548fa82bde9afe6e24b9cad923d67c
yosev <yosmisyael7308@gmail.com>
2024-06-12 08:48:09 +0700

    feat: add active effect on submission nav link

--- resources/views/components/dashboard-sidebar.blade.php
+++ resources/views/components/dashboard-sidebar.blade.php
@@ -139,7 +139,7 @@
                     <li>
                         <a
                             href="{{ route('admin.submission.index') }}"
-                            class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                            class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 {{ request()->getUri() === route('admin.submission.index') ? 'bg-gray-100' : '' }}"
                         >Submission</a
                         >
                     </li>
