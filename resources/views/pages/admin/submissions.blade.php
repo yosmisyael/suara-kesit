474aeec4d0034c1566bb5c7faa7757e684d3f9b0
yosev <yosmisyael7308@gmail.com>
2024-06-12 09:09:57 +0700

    feat: add alert message on success operation

--- resources/views/pages/admin/submissions.blade.php
+++ resources/views/pages/admin/submissions.blade.php
@@ -3,11 +3,34 @@
     <x-slot:title>{{ $title }}</x-slot:title>
     <x-dashboard-shell>
 
-        <div>
+        <div class="relative">
             <header
                 class="mb-5 flex flex-col gap-1 text-black rounded-lg p-5 bg-gradient-to-l from-indigo-100 to-fuchsia-200 via-stone-100 shadow-lg text-slate-700">
                 <h1 class="text-4xl font-medium">{{ explode('|', $title)[1] }}</h1>
             </header>
+
+            @if(session('success'))
+                <div class="w-max left-1/2 -translate-x-1/2 absolute top-0 z-10">
+                    <div class="flex rounded-md bg-green-100 p-4 text-sm text-green-500" x-cloak x-show="showAlert"
+                         x-data="{ showAlert: true }">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                             class="mr-3 h-5 w-5 flex-shrink-0">
+                            <path fill-rule="evenodd"
+                                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                  clip-rule="evenodd"/>
+                        </svg>
+                        <div><b>Success Alert.</b> {{ session()->get('success') }}</div>
+                        <button class="ml-auto" x-on:click="showAlert = false">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                 class="h-5 w-5">
+                                <path
+                                    d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z"/>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            @endif
+
             <table class="w-full border-collapse bg-white text-left text-sm text-gray-500 shadow-md border-[1px] mt-5">
                 <thead class="bg-gradient-to-l from-indigo-100 to-fuchsia-100 via-stone-100">
                 <tr>
