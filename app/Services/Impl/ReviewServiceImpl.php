400e04495f8a91af34aef083205f0bc4ecbe57a9
yosev <yosmisyael7308@gmail.com>
2024-06-12 09:11:58 +0700

    feat: change create review service implementation

--- app/Services/Impl/ReviewServiceImpl.php
+++ app/Services/Impl/ReviewServiceImpl.php
@@ -2,7 +2,9 @@
 
 namespace App\Services\Impl;
 
+use App\Models\Post;
 use App\Models\Review;
+use App\Models\Submission;
 use App\Services\ReviewService;
 
 class ReviewServiceImpl implements ReviewService
@@ -11,6 +13,15 @@ class ReviewServiceImpl implements ReviewService
     public function create(array $data): bool
     {
         $review = new Review($data);
+
+        $postId = Submission::query()->find($data['submission_id'])->post_id;
+
+        if ($data['status'] === 'approved') {
+            Post::query()->find($postId)->update([
+                'is_published' => true
+            ]);
+        }
+
         return $review->save();
     }
 }
