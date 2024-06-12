4caf258debaba7caf0da0c65c275d103fe20ced2
yosev <yosmisyael7308@gmail.com>
2024-06-12 09:41:28 +0700

    feat: add submission relations method

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Str;
 
 class Post extends Model
@@ -38,5 +39,10 @@ class Post extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function submission(): HasMany
+    {
+        return $this->hasMany(Submission::class);
     }
 }
