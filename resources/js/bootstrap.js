74e6ac36e331041319ae21d20154e730976f2af9
yosev <yosmisyael7308@gmail.com>
2024-06-14 21:06:47 +0700

    feat: store apex chart object in alpine store

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,11 +2,16 @@ import axios from 'axios';
 import Alpine from 'alpinejs';
 import 'flowbite/dist/flowbite.min.js';
 import Precognition from 'laravel-precognition-alpine';
+import ApexCharts from "apexcharts";
 
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 Alpine.plugin(Precognition);
+Alpine.store('ApexCharts', {
+    constructor: ApexCharts,
+});
 Alpine.start();
 
 window.Alpine = Alpine;
+window.ApexCharts = ApexCharts;
