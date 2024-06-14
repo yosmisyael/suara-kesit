4bfe6896f2167c0adf1e30384aecaa1a5c67ddf5
yosev <yosmisyael7308@gmail.com>
2024-06-14 21:28:39 +0700

    feat: change dashboard background color

--- resources/views/components/dashboard-shell.blade.php
+++ resources/views/components/dashboard-shell.blade.php
@@ -1,4 +1,4 @@
-<div class="antialiased bg-white min-h-screen">
+<div class="antialiased bg-base min-h-screen">
     <x-dashboard-navbar></x-dashboard-navbar>
     <x-dashboard-sidebar></x-dashboard-sidebar>
     <main class="p-4 md:ml-64 h-auto pt-20">
