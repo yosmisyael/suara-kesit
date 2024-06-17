c427cf0ac3ca8542cc67a3e9bcb2f996667f5a26
yosev <yosmisyael7308@gmail.com>
2024-06-18 00:05:18 +0700

    feat: update create method implementaion

--- app/Services/Impl/ReviewServiceImpl.php
+++ app/Services/Impl/ReviewServiceImpl.php
@@ -2,26 +2,29 @@
 
 namespace App\Services\Impl;
 
+use App\Enums\Status;
 use App\Models\Post;
 use App\Models\Review;
 use App\Models\Submission;
 use App\Services\ReviewService;
+use Illuminate\Support\Facades\DB;
 
 class ReviewServiceImpl implements ReviewService
 {
-
     public function create(array $data): bool
     {
-        $review = new Review($data);
+        return DB::transaction(function () use ($data) {
+            $review = new Review($data);
 
-        $postId = Submission::query()->find($data['submission_id'])->post_id;
+            $postId = Submission::query()->find($data['submission_id'])->getAttribute('post_id');
 
-        if ($data['status'] === 'approved') {
-            Post::query()->find($postId)->update([
-                'is_published' => true
-            ]);
-        }
+            if (Status::from($data['status']) === Status::Approved) {
+                Post::query()->find($postId)->update([
+                    'is_published' => true
+                ]);
+            }
 
-        return $review->save();
+            return $review->save();
+        });
     }
 }
---
+++ tests/Feature/Services/ReviewServiceImplTest.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Tests\Services\Impl;
+
+test('$END$', function () {
+
+});
