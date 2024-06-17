aad2a7a4014ca309cd6fad8c471b050c09db6e1e
yosev <yosmisyael7308@gmail.com>
2024-06-18 00:05:59 +0700

    test: create unit test for review service

--- tests/Feature/Services/ReviewServiceImplTest.php
+++ tests/Feature/Services/ReviewServiceImplTest.php
@@ -1,7 +1,23 @@
 <?php
 
-namespace Tests\Services\Impl;
+use App\Models\Post;
+use App\Models\Submission;
+use App\Services\ReviewService;
+use Database\Seeders\DatabaseSeeder;
 
-test('$END$', function () {
+describe('ReviewServiceImpl', function () {
+    beforeEach(function () {
+        $this->reviewService = app()->make(ReviewService::class);
+        $this->seed(DatabaseSeeder::class);
+    });
 
+    it('should be able to create a new review', function () {
+        $submissionId = Submission::query()->first()->getAttribute('id');
+        expect($this->reviewService->create([
+            'submission_id' => $submissionId,
+            'status' => 'approved',
+            'note' => 'great'
+        ]))->toBeTrue()
+            ->and(Post::query()->whereHas('submissions')->first()->getAttribute('is_published'))->toBeTrue();
+    });
 });
