f16775d71770064334a657b482506334de68e40e
yosev <yosmisyael7308@gmail.com>
2024-06-19 13:13:49 +0700

    feat: change relation to review to become has one

--- app/Models/Submission.php
+++ app/Models/Submission.php
@@ -6,7 +6,7 @@ use DateTimeInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Support\Facades\Storage;
 
 /**
@@ -33,9 +33,9 @@ class Submission extends Model
         return $this->belongsTo(Post::class);
     }
 
-    public function reviews(): HasMany
+    public function reviews(): HasOne
     {
-        return $this->hasMany(Review::class);
+        return $this->hasOne(Review::class);
     }
 
     protected static function booted(): void
