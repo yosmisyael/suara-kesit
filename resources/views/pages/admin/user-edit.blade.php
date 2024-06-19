9cdd87968c3242f660639fea5717159fac995ac9
yosev <yosmisyael7308@gmail.com>
2024-06-19 11:48:34 +0700

    feat: remove switch user role feature

--- resources/views/pages/admin/user-edit.blade.php
+++ resources/views/pages/admin/user-edit.blade.php
@@ -1,153 +1,84 @@
 <x-app>
     <x-slot:title>{{ $title }}</x-slot:title>
     <x-dashboard-shell>
-        <div x-data="{ showModal: false }" class="relative">
-            <header class="text-black">
-                <h1 class="text-5xl font-black md:text-7xl">User Role Edit</h1>
-            </header>
-            <div class="w-fit left-1/2 -translate-x-1/2 absolute z-10">
-                @if(session('success'))
-                    <div class="flex rounded-md bg-green-100 p-4 text-sm text-green-500" x-cloak x-show="showAlert" x-data="{ showAlert: true }">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="mr-3 h-5 w-5 flex-shrink-0">
-                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+        <header class="text-black">
+            <h1 class="text-5xl font-black md:text-7xl">User Role Edit</h1>
+        </header>
+        <div class="w-fit left-1/2 -translate-x-1/2 absolute z-10">
+            @if(session('success'))
+                <div class="flex rounded-md bg-green-100 p-4 text-sm text-green-500" x-cloak x-show="showAlert" x-data="{ showAlert: true }">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="mr-3 h-5 w-5 flex-shrink-0">
+                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+                    </svg>
+                    <div><b>Success Alert.</b> {{ session()->get('success') }} </div>
+                    <button class="ml-auto" x-on:click="showAlert = false">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
+                            <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
                         </svg>
-                        <div><b>Success Alert.</b> {{ session()->get('success') }} </div>
-                        <button class="ml-auto" x-on:click="showAlert = false">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
-                                <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
-                            </svg>
-                        </button>
-                    </div>
-                @endif
-            </div>
-
-            <div class="bg-white overflow-hidden shadow rounded-lg border">
-                <div class="px-4 py-5 sm:px-6">
-                    <h3 class="text-xl leading-6 font-medium text-gray-900">
-                        User Profile
-                    </h3>
-                    <p class="mt-1 max-w-2xl text-md text-gray-500">
-                        This is some information about the user.
-                    </p>
+                    </button>
                 </div>
-                <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
-                    <dl class="sm:divide-y sm:divide-gray-200">
-                        <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="text-sm font-medium text-gray-500">
-                                Full name
-                            </dt>
-                            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                {{ $user->name }}
-                            </dd>
-                        </div>
-                        <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="text-sm font-medium text-gray-500">
-                                Email address
-                            </dt>
-                            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                {{ $user->email }}
-                            </dd>
-                        </div>
-                        <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="text-sm font-medium text-gray-500">
-                                Role
-                            </dt>
-                            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                {{ $user->roles->first()->name }}
-                            </dd>
-                        </div>
-                        <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="text-sm font-medium text-gray-500">
-                                Join on
-                            </dt>
-                            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                {{ $user->created_at }}
-                            </dd>
-                        </div>
-                        <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="text-sm font-medium text-gray-500">
-                                Change role status
-                            </dt>
-                            <div class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                <button type="button" x-on:click="showModal = true"
-                                        class="rounded-lg border border-primary bg-primary px-5 py-2.5 text-center text-sm font-semibold text-white shadow-sm transition-all hover:border-primary focus:ring focus:ring-secondary">
-                                    @if($user->roles->first()->name === 'member')
-                                        Switch to author
-                                    @elseif($user->roles->first()->name === 'author')
-                                        Switch to member
-                                    @endif
-                                </button>
-                            </div>
-                        </div>
-                    </dl>
-                </div>
-            </div>
-
-            <a href="{{ $user->roles->first()->name === 'member' ? route('admin.user.member') : route('admin.user.author')  }}"
-               class="mt-10 inline-flex items-center gap-1.5 rounded-lg border border-primary bg-primary px-5 py-2.5 text-center text-sm font-medium text-white shadow-sm transition-all focus:ring focus:ring-secondary">
-                <i class="fa-solid fa-arrow-left text-white"></i>
-                Back
-            </a>
-            <template x-if="showModal">
-                <div>
-                    <div class="fixed z-50 top-0 left-0 right-0 h-screen bg-black opacity-20"></div>
-                    <div x-on:keydown.window.escape="showModal = false">
-
-                        <div x-cloak x-transition.opacity
-                             class="fixed inset-0 z-10 bg-secondary-700/50"></div>
-                        <div x-cloak x-transition
-                             class="fixed inset-0 z-50 flex items-center justify-center p-4 sm:p-0">
-                            <div class="mx-auto w-full overflow-hidden rounded-lg bg-white shadow-xl sm:max-w-sm">
-                                <div class="relative p-5">
-                                    <div class="text-center">
-                                        <div
-                                            class="mx-auto mb-5 flex h-10 w-10 items-center justify-center rounded-full bg-yellow-100 text-yellow-500">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                 stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                      d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 019 9v.375M10.125 2.25A3.375 3.375 0 0113.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 013.375 3.375M9 15l2.25 2.25L15 12"/>
-                                            </svg>
-                                        </div>
-                                        <div>
-                                            <h3 class="text-lg font-medium text-secondary-900">Confirmation</h3>
-                                            <div class="mt-2 text-sm text-secondary-500">Do you want to make this user
-                                                @if($user->roles->first()->name === 'member')
-                                                    author?
-                                                @elseif($user->roles->first()->name === 'author')
-                                                    member? All post that belongs to this user will be set to private.
-                                                @endif
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="mt-5 flex justify-end gap-3">
-                                        <button type="button" x-on:click="showModal = false"
-                                                class="flex-1 rounded-lg border border-gray-300 bg-white px-4 py-2 text-center text-sm font-medium text-gray-700 shadow-sm transition-all hover:bg-gray-100 focus:ring focus:ring-gray-100 disabled:cursor-not-allowed disabled:border-gray-100 disabled:bg-gray-50 disabled:text-gray-400">
-                                            Cancel
-                                        </button>
-                                        <form action="{{ route('admin.user.update', ['id' => $user->id]) }}" method="post" class="flex-1">
-                                            @csrf
-                                            @method('PUT')
-                                            <input type="hidden" name="id" value="{{ $user->id }}">
-                                            @if($user->roles->first()->name === 'author')
-                                                <input type="hidden" name="role" value="member">
-                                            @elseif($user->roles->first()->name === 'member')
-                                                <input type="hidden" name="role" value="author">
-                                            @endif
-                                            <button type="submit"
-                                                    class="w-full rounded-lg border border-blue-500 bg-blue-500 px-4 py-2 text-center text-sm font-medium text-gray-700 shadow-sm transition-all hover:bg-blue-700 focus:ring focus:ring-gray-100 disabled:cursor-not-allowed disabled:border-gray-100 disabled:bg-gray-50 disabled:text-gray-400 text-white">
-                                                Confirm
-                                            </button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </template>
+            @endif
         </div>
+
+        <div class="bg-white overflow-hidden shadow rounded-lg border">
+            <div class="px-4 py-5 sm:px-6">
+                <h3 class="text-xl leading-6 font-medium text-gray-900">
+                    User Profile
+                </h3>
+                <p class="mt-1 max-w-2xl text-md text-gray-500">
+                    This is detail information about the user.
+                </p>
+            </div>
+            <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
+                <dl class="sm:divide-y sm:divide-gray-200">
+                    <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500">
+                            Full name
+                        </dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            {{ $user->name }}
+                        </dd>
+                    </div>
+                    <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500">
+                            Email address
+                        </dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            {{ $user->email }}
+                        </dd>
+                    </div>
+                    <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500">
+                            Role
+                        </dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            {{ $user->roles->first()->name }}
+                        </dd>
+                    </div>
+                    <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500">
+                            Join on
+                        </dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            {{ $user->created_at }}
+                        </dd>
+                    </div>
+                    <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500">
+                            Role
+                        </dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            {{ $user->roles->first()->name }}
+                        </dd>
+                    </div>
+                </dl>
+            </div>
+        </div>
+
+        <a href="{{ $user->roles->first()->name === 'member' ? route('admin.user.member') : route('admin.user.author')  }}"
+           class="mt-10 inline-flex items-center gap-1.5 rounded-lg border border-primary bg-primary px-5 py-2.5 text-center text-sm font-medium text-white shadow-sm transition-all focus:ring focus:ring-secondary">
+            <i class="fa-solid fa-arrow-left text-white"></i>
+            Back
+        </a>
     </x-dashboard-shell>
-
-
 </x-app>
