332548a1d14c38d157dc485e41ead935783114c3
yosev <yosmisyael7308@gmail.com>
2024-06-25 15:04:52 +0700

    feat: compress uploaded image size before saving

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -5,27 +5,35 @@ namespace App\Http\Controllers;
 use App\Enums\ImageType;
 use App\Http\Requests\UploadImageRequest;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Intervention\Image\Laravel\Facades\Image;
+use Mockery\Exception;
 
 class ImageController extends Controller
 {
     public function store(UploadImageRequest $request): array
     {
         $validated = $request->validated();
-
-        $path = '';
+        $path = null;
+        $file = Str::uuid()->toString() . '.' . $validated['image']->getClientOriginalExtension();
 
         switch (ImageType::from($validated['type'])) {
             case ImageType::Attachment:
-                $path = $validated['image']->storePublicly('attachments', 'public');
+                $path = storage_path('app/public/attachments') . "/$file";
                 break;
-
             case ImageType::Profile:
-                $path = $validated['image']->storePublicly('profiles', 'public');
+                $path = storage_path('app/public/profiles') . "/$file";
                 break;
         }
 
+        if (!$path) throw new Exception('An error occurred when saving image.');
+
+        Image::read($validated['image'])
+            ->scale(1200)
+            ->save($path, progressive: true);
+
         return [
-            'image_url' => Storage::disk('public')->url($path),
+            'image_url' => ImageType::from($validated['type']) === ImageType::Attachment ? Storage::disk('public')->url("attachments/$file") : Storage::disk('public')->url("profiles/$file"),
         ];
     }
 
