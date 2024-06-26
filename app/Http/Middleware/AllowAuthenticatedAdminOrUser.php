ef8240c6d87e754afb593b0b66359aadf2ee0731
yosev <yosmisyael7308@gmail.com>
2024-06-26 14:52:10 +0700

    feat: create middleware for admin and user

---
+++ app/Http/Middleware/AllowAuthenticatedAdminOrUser.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AllowAuthenticatedAdminOrUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Closure(Request): (Response) $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = auth()->check();
+        $admin = auth('admin')->check();
+
+        if (!$user && !$admin) abort(403);
+
+        return $next($request);
+    }
+}
