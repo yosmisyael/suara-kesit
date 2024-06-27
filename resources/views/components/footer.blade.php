703bfb8bf441b3933dae2a32a46b8595267c05e4
yosev <yosmisyael7308@gmail.com>
2024-06-27 16:53:13 +0700

    feat: create footer component

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,34 @@
+<footer class="absolute bottom-0 w-full bg-black text-white pt-20">
+    <div class="grid grid-cols-3 px-20 md:px-60 gap-10 mb-10">
+        <div class="col-span-3 md:col-span-1 flex flex-col md:flex-row items-center">
+            <img src="{{ asset('kesit.png') }}" alt="logo" class="w-40 md:w-20">
+            <h1 class="font-black md:text-3xl text-4xl uppercase text-center">Suara Kesit</h1>
+        </div>
+        <div class="col-span-3 md:col-span-1 text-center md:text-left">
+            <h2 class="font-bold text-xl uppercase">About Us</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda blanditiis doloremque esse eum
+                exercitationem explicabo quod sed temporibus voluptatem voluptatibus.</p>
+        </div>
+        <div class="col-span-3 md:col-span-1 text-center md:text-left">
+            <h2 class="font-bold text-xl uppercase">Follow Us</h2>
+            <div class="flex gap-2">
+                <a href="">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-10 fill-white hover:fill-red-600" viewBox="0 0 24 24">
+                        <path
+                            d="M20.947 8.305a6.53 6.53 0 0 0-.419-2.216 4.61 4.61 0 0 0-2.633-2.633 6.606 6.606 0 0 0-2.186-.42c-.962-.043-1.267-.055-3.709-.055s-2.755 0-3.71.055a6.606 6.606 0 0 0-2.185.42 4.607 4.607 0 0 0-2.633 2.633 6.554 6.554 0 0 0-.419 2.185c-.043.963-.056 1.268-.056 3.71s0 2.754.056 3.71c.015.748.156 1.486.419 2.187a4.61 4.61 0 0 0 2.634 2.632 6.584 6.584 0 0 0 2.185.45c.963.043 1.268.056 3.71.056s2.755 0 3.71-.056a6.59 6.59 0 0 0 2.186-.419 4.615 4.615 0 0 0 2.633-2.633c.263-.7.404-1.438.419-2.187.043-.962.056-1.267.056-3.71-.002-2.442-.002-2.752-.058-3.709zm-8.953 8.297c-2.554 0-4.623-2.069-4.623-4.623s2.069-4.623 4.623-4.623a4.623 4.623 0 0 1 0 9.246zm4.807-8.339a1.077 1.077 0 0 1-1.078-1.078 1.077 1.077 0 1 1 2.155 0c0 .596-.482 1.078-1.077 1.078z"></path>
+                        <circle cx="11.994" cy="11.979" r="3.003"></circle>
+                    </svg>
+                </a>
+                <a href="">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-10 fill-white hover:fill-red-600" viewBox="0 0 24 24">
+                        <path
+                            d="M21.593 7.203a2.506 2.506 0 0 0-1.762-1.766C18.265 5.007 12 5 12 5s-6.264-.007-7.831.404a2.56 2.56 0 0 0-1.766 1.778c-.413 1.566-.417 4.814-.417 4.814s-.004 3.264.406 4.814c.23.857.905 1.534 1.763 1.765 1.582.43 7.83.437 7.83.437s6.265.007 7.831-.403a2.515 2.515 0 0 0 1.767-1.763c.414-1.565.417-4.812.417-4.812s.02-3.265-.407-4.831zM9.996 15.005l.005-6 5.207 3.005-5.212 2.995z"></path>
+                    </svg>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="py-5 md:px-60 px-20 border-t-2 border-white text-center w-1/2 mx-auto">
+        © 2024 Suara Kesit by Yosev
+    </div>
+</footer>
