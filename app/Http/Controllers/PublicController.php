d7c11c77e7548352107ae7c17c801c95d86eb4d1
yosev <yosmisyael7308@gmail.com>
2024-06-28 15:31:45 +0700

    feat: creat method to show post

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -2,16 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\PostService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class PublicController extends Controller
 {
+    public function __construct(protected PostService $postService)
+    {
+    }
+
     public function index(): Response
     {
+        $posts = $this->postService->getPublished();
+
         return response()
             ->view('pages.public.home', [
-                'title' => 'Home'
+                'title' => 'Home',
+                'posts' => $posts
+            ]);
+    }
+
+    public function list(Request $request): Response
+    {
+        $posts = $this->postService->getPublished();
+
+        return response()
+            ->view('pages.public.posts', [
+                'title' => 'Post List',
+                'posts' => $posts
+            ]);
+    }
+
+    public function show(string $id): Response
+    {
+//        $post = $this->postService->getById($id);
+
+        return response()
+            ->view('pages.public.post', [
+                'title' => 'Post title',
+//                'post' => $post
             ]);
     }
 }
