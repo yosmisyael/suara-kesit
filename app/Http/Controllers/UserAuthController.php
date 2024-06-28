78902ae219b60f1b197c8cfa992498b7adc94bf2
yosev <yosmisyael7308@gmail.com>
2024-06-28 16:05:48 +0700

    feat: implement methods for user auth and register

--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -2,9 +2,62 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\UserAuthRequest;
+use App\Http\Requests\UserRegisterRequest;
+use App\Services\UserAuthService;
+use App\Services\UserService;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
 
 class UserAuthController extends Controller
 {
-    //
+    public function __construct(protected readonly UserAuthService $adminAuthService, protected readonly UserService $userService)
+    {
+    }
+
+    public function register(): Response
+    {
+        return response()
+            ->view('pages.user.registration', [
+                'title' => 'User Registration'
+            ]);
+    }
+
+    public function store(UserRegisterRequest $request): RedirectResponse
+    {
+        $validated = $request->validated();
+
+        $result = $this->userService->create($validated);
+
+        if (!$result) return redirect()->back()->withErrors(['error' => 'An error occurred when registering user.'])->withInput();
+
+        return redirect(route('user.auth.login'))
+            ->with('success', 'You have successfully registered. Now please login with your new account');
+    }
+
+    public function login(): Response
+    {
+        return response()->view('pages.user.login', [
+            'title' => 'User Login'
+        ]);
+    }
+
+    public function authenticate(UserAuthRequest $request): RedirectResponse
+    {
+        $validated = $request->validated();
+        $result = $this->adminAuthService->login($validated['email'], $validated['password']);
+
+        if (!$result)
+            return redirect(route('user.auth.login'))
+                ->withErrors(['error' => 'Username or password is wrong.'])
+                ->withInput();
+
+        return redirect(route('user.dashboard'));
+    }
+
+    public function logout(): RedirectResponse
+    {
+        $this->adminAuthService->logout();
+        return redirect(route('user.auth.login'));
+    }
 }
