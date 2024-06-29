eb6b4972bede14a23d6728bdc727e81c80962039
yosev <yosmisyael7308@gmail.com>
2024-06-29 17:59:23 +0700

    feat: create user DTO

--- app/DTO/UserDTO.php
+++ app/DTO/UserDTO.php
@@ -2,7 +2,21 @@
 
 namespace App\DTO;
 
+use App\Traits\DataTransferObject;
+
 class UserDTO
 {
+    use DataTransferObject;
 
+    public string $email;
+    public string $password;
+    public string $username;
+    public string $name;
+    public string $role;
+
+    public function __construct(array $data)
+    {
+        $this->fill($data);
+        $this->setDefault('role', 'member');
+    }
 }
