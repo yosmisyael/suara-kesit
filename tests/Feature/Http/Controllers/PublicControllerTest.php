5b2a5262e9768e6aa1830fc95c563d6fba74769f
yosev <yosmisyael7308@gmail.com>
2024-07-01 00:41:17 +0700

    test: create unit tests for public controller

--- tests/Feature/Http/Controllers/PublicControllerTest.php
+++ tests/Feature/Http/Controllers/PublicControllerTest.php
@@ -1,7 +1,33 @@
 <?php
 
-test('example', function () {
-    $response = $this->get('/');
+describe('PublicController', function() {
+    it('should be able to access home page', function () {
+        $this->get(route('public.home'))
+            ->assertOk()
+            ->assertViewIs('pages.public.home');
+    });
 
-    $response->assertStatus(200);
+    it('should be able to access about page', function () {
+        $this->get(route('public.about'))
+            ->assertOk()
+            ->assertViewIs('pages.public.about');
+    });
+
+    it('should be able to access contact page', function () {
+        $this->get(route('public.contact'))
+            ->assertOk()
+            ->assertViewIs('pages.public.contact');
+    });
+
+    it('should be able to access posts page', function () {
+        $this->get(route('public.posts'))
+            ->assertOk()
+            ->assertViewIs('pages.public.posts');
+    });
+
+    it('should be able to access post page', function () {
+        $this->get(route('public.post', ['id' => 'test']))
+            ->assertOk()
+            ->assertViewIs('pages.public.post');
+    });
 });
